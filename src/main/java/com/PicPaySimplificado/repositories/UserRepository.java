c8aa3e7859a573ed5188995b5e0b287c562c9120
Tiago Simao <110579953+TiagoSimaodev@users.noreply.github.com>
2023-09-18 19:41:00 -0300

    feat: create UserRepository

---
+++ src/main/java/com/PicPaySimplificado/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.PicPaySimplificado.repositories;
+
+import com.PicPaySimplificado.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional <User> findUserByDocument(String document);
+    Optional <User> findUserByDocument(Long id);
+
+}
