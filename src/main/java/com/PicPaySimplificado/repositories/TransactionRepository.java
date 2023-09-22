7f1888715fd9deebcae538b8c96f4c7a1fc8e39c
Tiago Simao <110579953+TiagoSimaodev@users.noreply.github.com>
2023-09-21 23:40:51 -0300

    feat: creat TransactionRepository

---
+++ src/main/java/com/PicPaySimplificado/repositories/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.PicPaySimplificado.repositories;
+
+
+import com.PicPaySimplificado.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
